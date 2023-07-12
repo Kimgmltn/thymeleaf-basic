96bf8f5c83a9f421c56f6bb739ac5b54cbb4b8e8
khs <T1900843@partner.tmapmobility.com>
2023-07-12 16:12:26 +0900

    타임리프 유틸리티 객체와 날짜

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -49,6 +50,14 @@ public class BasicController {
         return "basic/variable";
     }
 
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    static class User{
+        private String username;
+        private int age;
+    }
+
     @GetMapping("/basic-objects")
     public String basicObjects(HttpSession session) {
         session.setAttribute("sessionData", "Hello Session");
@@ -62,11 +71,9 @@ public class BasicController {
         }
     }
 
-    @Data
-    @AllArgsConstructor
-    @NoArgsConstructor
-    static class User{
-        private String username;
-        private int age;
+    @GetMapping("/date")
+    public String date(Model model) {
+        model.addAttribute("localDateTime", LocalDateTime.now());
+        return "basic/date";
     }
 }
